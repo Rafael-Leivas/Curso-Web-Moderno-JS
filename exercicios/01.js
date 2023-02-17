25a451ab7c8bb3c8700b4382e1382d109dd73a5d
Rafael Leivas <rafaelblsteam@gmail.com>
2023-02-17 11:49:13 -0300

    inicio exercícios

---
+++ exercicios/01.js
@@ -0,0 +1,38 @@
+//Exercício 1
+/* function cumprimentar(nome){
+    console.log(`Olá ${nome}!`)
+    
+    cumprimentar('Felipe')
+} */
+
+//Exercício 2
+/*function idadeEmAnos(idade){
+    console.log(`Sua quantidade de dias na Terra é de ${idade * 365}.`)
+}
+
+idadeEmAnos(25)*/
+
+//Exercício 3
+/*function calcularSalario(horasTrabalhadas, valorHora){
+    console.log(`Salário igual a R$${horasTrabalhadas * valorHora}.`)
+}
+
+calcularSalario(150, 40.5)*/
+
+//Exercício 4
+//desisti
+
+//exercício 5
+/*function maiorOuIgual(numero1,numero2){
+    if(numero1 === numero2){
+        console.log('true')
+    }else{
+        console.log('false')
+    }
+}
+
+maiorOuIgual(0,0)
+maiorOuIgual(0,'0')
+maiorOuIgual(5,1)*/
+
+//Exercício 6
